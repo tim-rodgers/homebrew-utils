04bb98fc72f07cebc624ee2a1f350ef42b310977
Timothy Rodgers <rodgers.timothy@gmail.com>
2019-01-20 10:30:47 +0000

    fix symlink

--- Formula/awsassume@0.2.0.rb
+++ Formula/awsassume@0.2.0.rb
@@ -1,10 +1,10 @@
 require "language/go"
 
-class AwsassumeAT010 < Formula
+class AwsassumeAT020 < Formula
 
 desc "A utility to make assuming roles with the AWS CLI easier"
-url "https://github.com/tim-rodgers/awsassume/releases/download/v0.1/awsassume.tar.gz"
-sha256 "7d8cc12cdc72a5c0f3aff3f6e583db7deef53640262b37879808918c02a1a115"
+url "https://github.com/tim-rodgers/awsassume/releases/download/v0.2/awsassume.tar.gz"
+sha256 "383ac0c00f2b0ac9305d3ac1d65cecc0d17eda1c53d15e3515c6034ad9d5c56a"
 head "https://github.com/tim-rodgers/awsassume"
 
 def install
