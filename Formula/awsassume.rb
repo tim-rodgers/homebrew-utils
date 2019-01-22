5ba442cf493ca76d2aab4ecc79b356fcb588b4c2
tim-rodgers <rodgers.timothy@gmail.com>
2019-01-22 13:01:23 +0000

    Update and rename awsassume@0.1.0.rb to awsassume.rb

--- Formula/awsassume.rb
+++ Formula/awsassume.rb
@@ -1,10 +1,10 @@
 require "language/go"
 
-class AwsassumeAT010 < Formula
+class Awsassume < Formula
 
 desc "A utility to make assuming roles with the AWS CLI easier"
-url "https://github.com/tim-rodgers/awsassume/releases/download/v0.1/awsassume.tar.gz"
-sha256 "7d8cc12cdc72a5c0f3aff3f6e583db7deef53640262b37879808918c02a1a115"
+url "https://github.com/tim-rodgers/awsassume/releases/download/v0.2.1/awsassume.tar.gz"
+sha256 "7008479688d8b0cca8a6c4d178176eecc8d182376b31a7d3d15da98c5c2fc99e"
 head "https://github.com/tim-rodgers/awsassume"
 
 def install
